5cc94e505c9a40e82edb46ed31280dbc3e32d9b3
Renz <lorenztuboro00@gmail.com>
2024-10-30 15:58:55 +0800

    Dashboard modified to create user entry for backend.

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,8 +1,29 @@
 import { useUser } from "@clerk/clerk-react";
+import axios from "axios";
+import { DateTime } from "luxon";
+import { useEffect } from "react";
 import { Link, Outlet } from "react-router-dom";
 
+const link = import.meta.env.VITE_BACKEND_URL;
+
 export default function Dashboard() {
-  const { isLoaded } = useUser();
+  const { isLoaded, isSignedIn, user } = useUser();
+
+  // Create local entry
+  useEffect(() => {
+    const createUser = async () => {
+      const timezone = DateTime.now().zoneName;
+      await axios.post(`${link}/users/createUser`, {
+        id: user?.id,
+        username: user?.username,
+        timezone: timezone,
+      });
+    };
+
+    if (isSignedIn) {
+      createUser();
+    }
+  }, [isSignedIn, user?.id, user?.username]);
 
   if (!isLoaded) return null;
 
